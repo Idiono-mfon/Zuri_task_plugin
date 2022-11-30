51babefe73a378e8d9f46071b54208269de9dec3
Anthony <olympnetwork@gmail.com>
2022-11-30 13:41:33 +0100

    chore: update resources

--- backend/src/initialize.js
+++ backend/src/initialize.js
@@ -1,3 +1,5 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
@@ -6,8 +8,14 @@ import notFoundHandler from './middleware/application/notFoundHandler.js';
 import errorHandler from './middleware/application/errorHandler.js';
 import config from './config/index.js';
 import routes from './routes/index.js';
-import swaggerUI from 'swagger-ui-express'
-import swaggerJsDoc from'swagger-jsdoc'
+import swaggerUI from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+const rootDir = path.dirname(path.dirname(__dirname));
 
 const app = express();
 
@@ -32,12 +40,12 @@ const options = {
 
 const specs = swaggerJsDoc(options);
 
-
 app.use(helmet());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(morgan(config.env.isProduction ? 'common' : 'dev'));
+app.use('/static', express.static(path.join(rootDir, 'client/dist')));
 app.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(specs));
 app.use(routes);
 app.use(notFoundHandler);
